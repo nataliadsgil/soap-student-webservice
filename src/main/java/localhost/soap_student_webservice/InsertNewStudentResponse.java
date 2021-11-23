30cc792226b57e1f46fb2551b4989e667a0dc322
Natália Gil <nataliadasilva.dev@gmail.com>
2021-11-23 16:02:20 -0300

    change port

--- src/main/java/localhost/soap_student_webservice/InsertNewStudentResponse.java
+++ src/main/java/localhost/soap_student_webservice/InsertNewStudentResponse.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.11.23 às 03:31:25 PM BRT 
+// Gerado em: 2021.11.23 às 03:55:32 PM BRT 
 //
 
 
