7523173abb84f9cc420d49ef8dc732ae99b4609e
Natália Gil <nataliadasilva.dev@gmail.com>
2021-11-23 01:29:57 -0300

    feat: initial commit

---
+++ src/main/java/com/nataliagil/soap_student_webservice/SoapStudentWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nataliagil.soap_student_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapStudentWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapStudentWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nataliagil/soap_student_webservice/SoapStudentWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataliagil.soap_student_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapStudentWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
