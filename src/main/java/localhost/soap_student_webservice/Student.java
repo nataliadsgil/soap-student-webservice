65c059683575983a51b29910bec1c80ffffd8164
Natália Gil <nataliadasilva.dev@gmail.com>
2021-11-23 15:32:40 -0300

    update

--- src/main/java/localhost/soap_student_webservice/Student.java
+++ src/main/java/localhost/soap_student_webservice/Student.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.11.23 às 02:39:30 AM BRT 
+// Gerado em: 2021.11.23 às 03:31:25 PM BRT 
 //
 
 
